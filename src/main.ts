688650f6a6d826a0f99eeb5912285d5caea4b105
Areeb Khan <mohammad.munib62@gmail.com>
2025-07-14 01:07:51 +0500

    new

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   app.use(cookieParser())
 
   app.enableCors({
-    origin: '*',
+    origin: 'https://event-coordination-client.vercel.app',
     credentials: true,
   });
 
